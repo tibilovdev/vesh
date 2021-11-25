21962d53bf49d9cf8073954244e8b1b4aac6aa07
tibilovdev <tibilovdev@gmail.com>
2021-11-25 14:53:37 +0300

    создание проекта и добавление файлов для домашней страницы

---
+++ src/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ГОЛОВНЫЕ УБОРЫ</h1>
+            <span className="subtitle">Купить Сейчас</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
