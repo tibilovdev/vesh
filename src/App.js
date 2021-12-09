9bfeadafa2e9a03e9cbc27fa08940b0c36679ed2
tibilovdev <tibilovdev@gmail.com>
2021-12-09 11:43:10 +0300

    обновил menu-items в плане навигации withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log('dsfdsf', props);
+  return (
+    <div>
+      <h1>HAtS Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/directory/directory.component.jsx
+++ src/components/menu-item/directory/directory.component.jsx
@@ -47,12 +47,13 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
+        {this.state.sections.map(({ title, id, imageUrl, size, linkUrl }) => (
           <MenuItem
             key={id}
             title={title.toUpperCase()}
             imageUrl={imageUrl}
             size={size}
+            linkUrl={linkUrl}
           />
         ))}
       </div>
