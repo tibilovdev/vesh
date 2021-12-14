a4f78ecc5926c6515e2ba68285833cfb9cde7b99
tibilovdev <tibilovdev@gmail.com>
2021-12-14 21:07:39 +0300

    добавил shop page, collection preview и collection item компоненты

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
 
 const HatsPage = (props) => {
   console.log('dsfdsf', props);
@@ -16,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
