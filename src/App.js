20cbe9127685c0570d0e151a563fe09dacc169ab
tibilovdev <tibilovdev@gmail.com>
2021-12-15 09:51:53 +0300

    добавил Header компонент с линками

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.component';
-
-const HatsPage = (props) => {
-  console.log('dsfdsf', props);
-  return (
-    <div>
-      <h1>HAtS Page</h1>
-    </div>
-  );
-};
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
